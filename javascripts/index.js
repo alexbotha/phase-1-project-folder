6a3e79bad75146f16ceecea84959ac017e312a98
alexbotha <alexbotha@hotmail.co.uk>
2022-08-24 11:35:26 +0100

    "Multiple changes to the original project plan.

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,1 +1,35 @@
-//alert("hello")
+const mainDiv = () => document.getElementById("main")
+
+document.addEventListener("DOMContentLoaded", () => {
+  renderHomePage();
+  fetchDogData();
+  
+  //Event listeners
+  function fetchDogData(){
+  const dogImageUrl = "https://dog.ceo/api/breeds/image/random"
+  fetch(dogImageUrl)
+  .then(res => res.json())
+  .then(data => console.log(data))
+}
+
+//Event Handlers
+function renderHomePage(){
+  resetMainDiv();
+
+  const h1 = document.createElement("h1")
+  const p = document.createElement("p")
+
+  h1.innerText = "emoji"
+  h1.style.marginTop = "0"
+  p.innerText = "Welcome to random dogs! Click the button below to see a photo of a good doggo"
+
+  mainDiv().appendChild(h1);
+  mainDiv().appendChild(p);
+}
+
+//Helpers
+function resetMainDiv(){
+  mainDiv().innerHTML = ""
+}
+
+})

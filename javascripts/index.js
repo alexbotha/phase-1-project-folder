648436be14d9023b1f39f8aa539c417bc84d3cf3
alexbotha <alexbotha@hotmail.co.uk>
2022-08-25 14:19:52 +0100

    added second section search bar

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,54 +1,54 @@
 //Event listener - DOMContent Loaded 
-
-  
   document.addEventListener('DOMContentLoaded', (event) => { 
 
+    const randomDogElement = document.querySelector(".random-dog")
+    const dogAPI = "https://dog.ceo/api/breeds/image/random/4"
     
-    const catAPI = "https://dog.ceo/api/breeds/image/random/4"
-    const randomCatElement = document.querySelector(".random-cats")
-    const goButton = document.querySelector(".go-button")
+    
+    const pawBtn = document.querySelector(".pawBtn")
+    
+    const listBreeds = "https://dog.ceo/api/breeds/list/all"
   
-    //Fetch request of dog images from the API
-    function getRandomCat(){
-      randomCatElement.innerHTML = ""
-      fetch(catAPI)
+//Fetch request of dog images from the API
+    function getRandomDog(){
+      randomDogElement.innerHTML = ""
+      fetch(dogAPI)
       .then((res) => res.json())
-      .then((passJsonData) => renderCatImages(passJsonData))
+      .then((passJsonData) => renderDogImages(passJsonData))
     }
+    console.log(getRandomDog)
+    console.log(randomDogElement)
+    console.log(dogAPI)
     
-    //Building a function that takes in the argument of passCatAPIData so we can render it onto the DOM
-    function renderCatImages(recievesThePassedJsonData){
+    function renderDogImages(recievesThePassedJsonData){
       
-      //Using dot notation to select the json data that has the key of message I use the forEach array method to irate over catImages which contains the API data, in this case, the images
-  recievesThePassedJsonData.message.forEach(catImage => {
-    
-    //Following on from using arrow syntax I have now started to create the structure of the images using columns to contain the API data
+    recievesThePassedJsonData.message.forEach(iteratedDogImage => {    
     const columnElement = document.createElement("div");
     columnElement.classList.add('column');
-    console.log(columnElement)
-    
-
+        
     const cardElement = document.createElement("div");
     cardElement.classList.add("card");
     columnElement.appendChild(cardElement);
-    
+        
     const cardImageElement = document.createElement("div");
     cardImageElement.classList.add("card-image");
     cardElement.appendChild(cardImageElement);
     
-    const figureElement = document.createElement("figure");
-    figureElement.classList.add('image');
-    cardImageElement.appendChild(figureElement)
+    const figureImgElement = document.createElement("figure");
+    figureImgElement.classList.add("figure");
+    cardImageElement.appendChild(figureImgElement)
     
-    const imageElement = document.createElement("img")
-    imageElement.src = catImage;
-    figureElement.appendChild(imageElement);
+    const imageLink = document.createElement("img")
+    imageLink.classList.add("imageLink")
+    imageLink.src = iteratedDogImage;
+    figureImgElement.appendChild(imageLink);
     
-    randomCatElement.appendChild(columnElement);
-    
+//append the dynamic img dog containers to the column element
+    randomDogElement.appendChild(columnElement);
   });
 }
 
 //Event Listener - generating dog images 
-goButton.addEventListener("click", getRandomCat);
+    pawBtn.addEventListener("click", getRandomDog);
+
 })

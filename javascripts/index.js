152afb064368ec057b7bd17d80ff935f3b7200ae
alexbotha <alexbotha@hotmail.co.uk>
2022-08-30 15:25:58 +0100

    added breed filter

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,73 +1,83 @@
-//Event listener - DOMContent Loaded 
-  document.addEventListener('DOMContentLoaded', () => { 
+/////////////////Event listener - DOMContent Loaded/////////////
 
-/////////////////////////RANDOM DOG PHOTO FROM API///////////////////////////////////
+document.addEventListener("DOMContentLoaded", () => {
+  /////////////////////GLOBAL VARIABLES////////////////////////
 
-    const randomDogElement = document.querySelector(".random-dog")
-    const dogAPI = "https://dog.ceo/api/breeds/image/random/4"
-    const pawBtn = document.querySelector(".pawBtn")    
+  const imageContainer = document.querySelector(".image-container");
+  const dogAPI = "https://dog.ceo/api/breeds/image/random/4";
+  const pawBtn = document.querySelector(".pawBtn");
+  const submitBtn = document.getElementById("submit-section");
 
-    function getRandomDog(){
-      randomDogElement.innerHTML = ""
-       fetch(dogAPI)
+  /////////////////RANDOM DOG PHOTO FROM API//////////////
+
+  function getRandomDog() {
+    imageContainer.innerHTML = "";
+    fetch(dogAPI)
       .then((res) => res.json())
-      .then((passJsonData) => renderDogImages(passJsonData))
+      .then((passJsonData) => renderDogImages(passJsonData));
+  }
+
+  function renderDogImages(recievesThePassedJsonData) {
+    recievesThePassedJsonData.message.forEach((iterateOverDogImageObjects) => {
+      const columnElement = document.createElement("div");
+      columnElement.classList.add("column");
+
+      const cardElement = document.createElement("div");
+      cardElement.classList.add("card");
+      columnElement.appendChild(cardElement);
+
+      const cardImageElement = document.createElement("div");
+      cardImageElement.classList.add("card-image");
+      cardElement.appendChild(cardImageElement);
+
+      const figureImgElement = document.createElement("figure");
+      figureImgElement.classList.add("figure");
+      cardImageElement.appendChild(figureImgElement);
+
+      const imageLink = document.createElement("img");
+      imageLink.classList.add("imageLink");
+      imageLink.src = iterateOverDogImageObjects;
+      figureImgElement.appendChild(imageLink);
+
+      imageContainer.appendChild(columnElement);
+    });
+  }
+
+  pawBtn.addEventListener("click", getRandomDog);
+
+  //////////////////////BREED LIST FROM API ///////////////////
+
+  submitBtn.addEventListener("submit", (e) => {
+    imageContainer.innerHTML = "";
+    e.preventDefault();
+    fetch(`https://dog.ceo/api/breed/${e.target[0].value}/images/random/4`)
+      .then((res) => res.json())
+      .then((passData) => renderBreedImages(passData));
+
+    function renderBreedImages(getsData) {
+      getsData.message.forEach((dogBreedImage) => {
+        const columnElement = document.createElement("div");
+        columnElement.classList.add("column");
+
+        const cardElement = document.createElement("div");
+        cardElement.classList.add("card");
+        columnElement.appendChild(cardElement);
+
+        const cardImageElement = document.createElement("div");
+        cardImageElement.classList.add("card-image");
+        cardElement.appendChild(cardImageElement);
+
+        const figureImgElement = document.createElement("figure");
+        figureImgElement.classList.add("figure");
+        cardImageElement.appendChild(figureImgElement);
+
+        const imageLink = document.createElement("img");
+        imageLink.classList.add("imageLink");
+        imageLink.src = dogBreedImage;
+        figureImgElement.appendChild(imageLink);
+
+        imageContainer.appendChild(columnElement);
+      });
     }
-   
-    function renderDogImages(recievesThePassedJsonData) {
-    recievesThePassedJsonData.message.forEach(iterateOverDogImageObjects => {    
-    const columnElement = document.createElement("div");
-    columnElement.classList.add('column');
-        
-    const cardElement = document.createElement("div");
-    cardElement.classList.add("card");
-    columnElement.appendChild(cardElement);
-        
-    const cardImageElement = document.createElement("div");
-    cardImageElement.classList.add("card-image");
-    cardElement.appendChild(cardImageElement);
-    
-    const figureImgElement = document.createElement("figure");
-    figureImgElement.classList.add("figure");
-    cardImageElement.appendChild(figureImgElement)
-    
-    const imageLink = document.createElement("img")
-    imageLink.classList.add("imageLink")
-    imageLink.src = iterateOverDogImageObjects;
-    figureImgElement.appendChild(imageLink);
-
-    randomDogElement.appendChild(columnElement);
-  });  
-}
-
-//////////////////////BREED LIST FROM API /////////////////////////////////////////
-
-let breeds = []
-  
-  function getBreedNames() {
-      const breedUrl = 'https://dog.ceo/api/breeds/list/all'
-      fetch(breedUrl)
-      .then(res => res.json())
-      .then(res => {
-        breeds = Object.keys(res.message)
-        addBreedNamesToDom(breeds)
-        })
-      }
-      
-    getBreedNames()
-    
-    function addBreedNamesToDom(breeds){
-      const ul = document.querySelector("#dog-breeds")
-        breeds.map(breed => {
-          const li = document.createElement("li")
-          li.textContent = breed
-          ul.append(li)
-          li.append
-        })
-    }
-
-//Adding an event listener onto the paw button to generate images upon click. I pass getRandom dog but don't invoke it as I only want it to appear on the page when it has been clicked 
-pawBtn.addEventListener("click", getRandomDog);
-
+  });
 });
-
